e07bc1669dd36433cb1e07b5a5b9a690093b021b
ballouc <cmbalp@gmail.com>
2024-02-24 11:04:02 -0500

    Minor changes

--- src/windowData.tsx
+++ src/windowData.tsx
@@ -2,21 +2,36 @@
 export const windowData = [
     {
         title: "ABOUT",
-        content: `<div>
-            <p>This is my About Me section.</p>
+        content:        
+        `<div id="about-container">
+            <div id='about-header' class='window-content-header'>
+                <h1 id='about-name'>Cam Ballou</h1>
+                <p id='about-degree'>Bachelor of Arts in Computer Science and Cybersecurity Studies</p>
+            </div>
         </div>`
     },
     {
         title: "WORK",
-        content: `<div>
-        <p>This is the section detailing my past work.</p>
+        content: 
+        `<div id="work-container">
+            <div class="window-content-header">
+                <h1 id="work-header">Favorite Tools and Languages</h1>
+                <div class="tools-container">
+                    <button class="tool-btn" id="tool-js">JavaScript</button>
+                    <button class="tool-btn" id="tool-react">React</button>
+                    <button class="tool-btn" id="tool-python">Python</button>
+                </div>
+            </div>
+            <div id="portfolio-container">
+
+            </div>
         </div>`
     },
     {
         title: "CONTACT",
         content: `
         <div id="contact-container">
-            <div id="contact-info">
+            <div id="contact-info" class='window-content-header'>
                 <h1 id="contact-name">Cam Ballou</h1>
                 <p>cmbalbus@gmail.com</p>
             </div>
